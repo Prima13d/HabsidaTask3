0cb5b2ae82347a78f800d0fd7b1a35cd4b429a6b
KIM ALEXEY <prima@MacBook-Pro-KIM.local>
2025-07-02 23:36:35 +0900

    Finished Task2_1_4. Added class Dog and updated classes Timer and Animal cage

--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,6 +1,20 @@
 package app.model;
 
+import  org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+
+@Component
 public class Timer {
+    private final LocalTime sayTime;
+
+    public Timer() {
+        this.sayTime = LocalTime.now();
+    }
+
+    public LocalTime getSayTime() {
+        return sayTime;
+    }
 
     private Long nanoTime = System.nanoTime();
 
